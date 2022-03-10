734996db4b27d359708de1f3f8d2b99c7fae2bc1
devmarcos <devmarcoseduardo@gmail.com>
2022-03-09 21:35:16 -0300

    Calculadora js

---
+++ Js/scripts.js
@@ -0,0 +1,62 @@
+window.onload = function() {
+    //variaveis
+    var segundos = 00
+    var milesimo = 00
+
+    var acrescentarSegudos = document.getElementById('segundos')
+    var acrescentarMilesimo = document.getElementById('milesimo')
+
+    var buttonIniciar = document.getElementById('button-start')
+    var buttonParar = document.getElementById('button-stop')
+    var buttonreset = document.getElementById('button-reset')
+    
+    var Intervalo
+
+    //iniciando o cronometro
+    buttonIniciar.onclick = function() {
+
+        clearInterval(Intervalo)
+        Intervalo = setInterval(iniciarContagem, 10)
+        
+    }
+
+    //pausando o codigo
+    buttonParar.onclick = function() {
+        clearInterval(Intervalo)
+    }
+
+    //limpando cronometro
+    buttonreset.onclick = function() {
+        clearInterval(Intervalo)
+        segundos = '00'
+        milesimo = '00'
+        acrescentarSegudos.innerHTML = segundos
+        acrescentarMilesimo.innerHTML = milesimo
+    }
+
+    //função iniciar
+    function iniciarContagem() {
+        //começa acrescentando os milesimos
+        milesimo++
+
+        if(milesimo <= 9) {
+            acrescentarMilesimo.innerHTML = '0' + milesimo
+        }
+
+        if(milesimo > 9) {
+            acrescentarMilesimo.innerHTML = milesimo
+        }
+
+        if(milesimo > 99) {
+            console.log('segundos')
+            segundos++
+            acrescentarSegudos.innerHTML = '0' + segundos
+            milesimo = 0
+            acrescentarMilesimo.innerHTML = '0' + 0
+        }
+
+        if (segundos > 9) {
+            acrescentarSegudos.innerHTML = segundos
+        }
+    }
+}
